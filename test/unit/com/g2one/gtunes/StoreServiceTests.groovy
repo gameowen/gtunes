4c556fa0addc88e2055b73094eeda35b554354f1
rpieciorak <rpieciorak@gmail.com>
2010-07-12 00:02:23 +0200

    testing service layer using MockFor

--- test/unit/com/g2one/gtunes/StoreServiceTests.groovy
+++ test/unit/com/g2one/gtunes/StoreServiceTests.groovy
@@ -1,17 +1,68 @@
 package com.g2one.gtunes
 
 import grails.test.*
+import groovy.mock.interceptor.*
 
 class StoreServiceTests extends GrailsUnitTestCase {
-    protected void setUp() {
-        super.setUp()
-    }
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+   // http://grails-groovy.blogspot.com/2009/02/unit-testing-testing-service-in-3_07.html
 
-    void testSomething() {
+   // http://www.jarvana.com/jarvana/view/org/codehaus/groovy/groovy/1.7.2/groovy-1.7.2-sources.jar!/groovy/mock/interceptor/MockFor.groovy
 
-    }
+   def storeService
+
+   protected void setUp() {
+      super.setUp()
+        
+      // initialize service
+      // there is no dynamic injection
+      storeService = new StoreService()
+   }
+
+   protected void tearDown() {
+      super.tearDown()
+   }
+
+   void testInvalidCreditCard() {
+      // create mock for CreditCardCommand class
+      def ccMock = new MockFor(CreditCardCommand)
+
+      // demand the validate method exactly once,
+      // and programming it to return false
+      ccMock.demand.validate {
+         false
+      }
+
+      ccMock.use{
+         // validation will fail
+         // expecting exception to be thrown
+         shouldFail(IllegalStateException) {
+            storeService.purchaseAlbum(new User(), new CreditCardCommand(), [])
+         }
+      }
+   }
+
+   void testInvalidAlbumPayment() {
+      def ccMock = new MockFor(CreditCardCommand)
+      ccMock.demand.validate {
+         true
+      }
+
+      def apMock = new MockFor(AlbumPayment)
+      apMock.demand.with {
+         setUser {}
+         validate {
+            false
+         }
+      }
+
+      ccMock.use{
+         // validation will fail
+         // expecting exception to be thrown
+         shouldFail(IllegalStateException) {
+            storeService.purchaseAlbum(new User(), new CreditCardCommand(), [ apMock.proxyInstance() ])
+         }
+      }
+   }
+
 }
